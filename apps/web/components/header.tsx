d38c684354de06ac5b544d546a424015de893cf0
junayed-code <junayed.code@gmail.com>
2025-03-22 13:15:09 +0600

    feat: add a header component
    This header component represents the main header of the app

---
+++ apps/web/components/header.tsx
@@ -0,0 +1,13 @@
+import Container from "@components/container";
+
+function Header() {
+  return (
+    <header className="self-start px-4 py-3.5">
+      <Container>
+        <h4 className="text-3xl font-bold">Vidonuxt</h4>
+      </Container>
+    </header>
+  );
+}
+
+export default Header;
