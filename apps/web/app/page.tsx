3dd448095e28c1b858be6c0c04c3622a334c04b4
junayed-code <junayed.code@gmail.com>
2025-03-25 12:12:50 +0600

    feat: implement the root page of the application
    This is a simple page that contains a header with a sub-header and
    the `Playground` component.

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,25 @@
+import Container from "@components/container";
+import Playground from "@components/playground";
+
 export default function Home() {
   return (
-    <main>
-      <h1 className="text-5xl font-bold">Vidonuxt</h1>
-    </main>
+    <Container className="flex flex-col justify-center gap-12 md:gap-14 lg:gap-16">
+      <div className="-mt-20 space-y-4 sm:-mt-10 md:space-y-6 lg:space-y-8">
+        <h1 className="text-center text-4xl font-bold lg:text-[3.5rem]">
+          <span className="text-primary text-2xl lg:text-3xl">
+            Just Paste & Play
+          </span>
+          <br />
+          Stop Downloading. Start Streaming.
+        </h1>
+        <p className="text-center md:text-lg">
+          Supports various video formats. Watch anything directly in your
+          browser. Experience instant streaming without limitations. Enjoy
+          effortless playback of your favorite content.
+        </p>
+      </div>
+
+      <Playground />
+    </Container>
   );
 }
