91253ecdfdcb6e8984e563e399cfcec7cbcfe636
junayed-code <junayed.code@gmail.com>
2025-03-25 11:37:54 +0600

    feat: implement the main layout of the application
    The layout is created with a background image and the main header and
    all pages are put into a main html tag.
    
    Title, description, keywords, and some open-graph metadata are configured
    for improved SEO.

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,9 +1,23 @@
+import Image from "next/image";
 import type { Metadata } from "next";
+
+import Header from "@components/header";
+
+import watching from "../assets/watching.jpg";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Vidonuxt",
-  description: "Vidonuxt is a video streaming platform",
+  title: "Vidonuxt | Just Paste & Play",
+  description:
+    "Vidonuxt is a video streaming platform. Watch anything that you want to. Just paste and play.",
+  keywords: "video, streaming, watch, play, url, link, paste, stream, vidonuxt",
+  openGraph: {
+    type: "website",
+    title: "Vidonuxt | Just Paste & Play",
+    description:
+      "Vidonuxt is a video streaming platform. Watch anything that you want to. Just paste and play.",
+    siteName: "Vidonuxt",
+  },
 };
 
 export default function RootLayout({
@@ -13,7 +27,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="text-secondary relative grid min-h-dvh grid-rows-[auto_1fr]">
+        <Image
+          fill
+          alt="Background Image"
+          src={watching}
+          placeholder="blur"
+          className="-z-10 object-cover"
+        />
+        <div className="absolute inset-0 -z-10 bg-neutral-200/75" />
+        <Header />
+        <main className="grid grid-cols-1 grid-rows-1 px-4 py-14">
+          {children}
+        </main>
+      </body>
     </html>
   );
 }
